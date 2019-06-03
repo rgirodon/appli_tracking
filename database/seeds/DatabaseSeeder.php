6c7bfd5846a99827d12e6dddf691d402d6c45363
Thomas Ferrand <a.tom.hic3@gmail.com>
2019-06-03 16:33:00 +0200

    Seed room

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call([
             TeamsTableSeeder::class,
+            RoomSeeder::class
         ]);
         /*Eloquent::unguard();
         $this->call(UsersTablesSeeder::class);*/
