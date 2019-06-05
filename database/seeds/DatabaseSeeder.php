54171dc8acf07076798b87e00acc54fd514875db
PierreTSE <pierre--giraud@orange.fr>
2019-06-05 14:13:38 +0200

    RiddlesSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call([
+            TeamsTableSeeder::class,
+            RiddlesSeeder::class
+        ]);
+        /*Eloquent::unguard();
+        $this->call(UsersTablesSeeder::class);*/
+
     }
 }
