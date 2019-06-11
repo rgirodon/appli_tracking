7d02720fcc68323d7074cfcb068c81bc9da39147
antoine-peyrard <antoine.peyrard@telecom-st-etienne.fr>
2019-06-11 12:19:06 +0200

    suppression de création de team

--- database/seeds/TeamsSeeder.php
+++ database/seeds/TeamsSeeder.php
@@ -19,12 +19,5 @@ class TeamsSeeder extends Seeder
             'password' => bcrypt('1234'),
             'isGM' => true,
         ]);
-        foreach (range(1, 10) as $index) {
-            DB::table('teams')->insert([
-                'name' => $faker->colorName . '_' . random_int(1, 10),
-                'password' => bcrypt('a'),
-                'isGM' => false,
-            ]);
-        }
     }
 }
