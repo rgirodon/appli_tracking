410a1528cc3a254db10c7eba614fd7429f8d0032
PierreTSE <pierre--giraud@orange.fr>
2019-06-13 18:26:50 +0200

    logo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,13 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{$title ?? config('app.name')}}</title>
+    <link rel="shortcut icon" type="image/png" href="{{url('images/favicon.jpg')}}"/>
     {{Html::style('css/app.css')}}
     {{Html::script('js/app.js')}}
 </head>
 <body>
 
-<header class="text-center">
-    <h1 class="my-auto"><a href="{{ url('/') }}">{{config('app.name')}}</a></h1>
+<header class="text-center py-3">
+    <div class="row justify-content-center">
+        <img src="{{url('/images/LOGO.png')}}" alt="logo de l'Escape Game" height="80">
+        <h1 class="my-auto ml-3"><a href="{{ url('/') }}">{{config('app.name')}}</a></h1>
+    </div>
 </header>
 
     <div class="sticky-top">
